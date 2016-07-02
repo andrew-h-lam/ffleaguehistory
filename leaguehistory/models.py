21b23435c61a0505bae886b0f4d56c40716b6392
Andrew Lam <andrew.h.lam@gmail.com>
2016-07-01 23:50:32 -0700

    updated models

--- leaguehistory/models.py
+++ leaguehistory/models.py
@@ -5,25 +5,28 @@ class Player(models.Model):
     last_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.first_name + self.last_name
+        return self.first_name + " " + self.last_name
 
 class Season(models.Model):
     name = models.CharField(max_length=50)
     year = models.IntegerField()
+    sub_id = models.IntegerField()
     buy_in = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 class Matchup(models.Model):
-    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+    season = models.ForeignKey(Season)
     week_number =  models.IntegerField()
-    home_team = models.ForeignKey(Player, on_delete=models.CASCADE)
+    playoff_round = models.IntegerField()
+    home_team = models.ForeignKey(Player, related_name='home_team')
     home_team_score = models.IntegerField()
-    away_team = models.ForeignKey(Player, on_delete=models.CASCADE)
+    away_team = models.ForeignKey(Player, related_name='away_team')
     away_team_score = models.IntegerField()
 
 class Payout(models.Model):
-    season = models.ForeignKey(Season, on_delete=models.CASCADE)
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    season = models.ForeignKey(Season)
+    player = models.ForeignKey(Player)
     description = models.CharField(max_length=50)
+    amount = models.IntegerField()
