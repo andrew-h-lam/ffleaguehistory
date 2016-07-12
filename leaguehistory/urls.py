0d58f060528746a26eb40e3b2749c535e2dad02a
Andrew Lam <andrew.h.lam@gmail.com>
2016-07-12 00:50:24 -0700

    latest changes

--- leaguehistory/urls.py
+++ leaguehistory/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^player$', views.player_index, name='player_index'),
 
     # /player/12/
-    #url(r'^player/(?P<player_id>[0-9]+)/$', views.player_detail, name='player_detail'),
+    url(r'^player/(?P<player_id>[0-9]+)/$', views.player_detail, name='player_detail'),
 
     #/season
     url(r'^season$', views.season_index, name='season_index'),
--- leaguehistory/views.py
+++ leaguehistory/views.py
@@ -4,8 +4,10 @@ from .models import Season, Player, Matchup, Payout
 
 def index(request):
     all_season = Season.objects.all()
+    all_player = Player.objects.order_by('first_name')
     context = {
         'all_season': all_season,
+        'all_player': all_player
     }
     return render(request, 'index.html', context)
 
@@ -13,6 +15,17 @@ def player_index(request):
     context = {
     }
     return render(request, 'player.html', context)
+
+def player_detail(request, player_id):
+    try:
+        player = Player.objects.get(id=player_id)
+        context = {
+            'player': player,
+        }
+    except Player.DoesNotExist:
+        raise Http404("Player does not exist")
+
+    return render(request, 'player/player_detail.html', context)
 
 def season_index(request):
     context = {
@@ -23,10 +36,14 @@ def season_index(request):
 def season_detail(request, season_id):
     try:
         season = Season.objects.get(id=season_id)
-        context = {
-            'season': season,
-        }
     except Season.DoesNotExist:
         raise Http404("Season does not exist")
 
+    players = Matchup.objects.order_by('home_team_id').values('home_team_id').distinct()
+    context = {
+        'season': season,
+        'players': players
+    }
+
     return render(request, 'season/season_detail.html', context)
+
